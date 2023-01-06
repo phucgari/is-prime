180073ab349bb7b1927ac33ccb6552731a318268
phucgari <phucgari@gmail.com>
2023-01-06 09:11:38 +0700

    done

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("What number do you want to check prime?");
+        Scanner scanner=new Scanner(System.in);
+        int num= scanner.nextInt();
+        boolean result = true;
+        if(num<2){
+            result=false;
+        }else {
+            for (int i = 2; i < num; i++) {
+                if(num%i==0){
+                    result=false;
+                    break;
+                }
+            }
+        }
+        if(result){
+            System.out.println(num+" is prime");
+        }else{
+            System.out.println(num+" is not prime");
+        }
     }
 }
